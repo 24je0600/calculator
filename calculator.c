731a6e304e33986cc12dc87d3b26c4e5e3219d8d
24je0600 <24je0600@iitism.ac.in>
2024-10-24 10:07:25 +0530

    code improvised

--- calculator.c
+++ calculator.c
@@ -74,7 +74,7 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("%d\n", 1/a);
+                printf("%f\n",(float) 1/a);
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
